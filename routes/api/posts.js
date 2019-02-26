9ea2351d33b9d14d65c09a63c9dd125a0933d062
oscar-becerril <oskbrrlz@gmail.com>
2019-02-25 21:09:45 -0600

    Update posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,16 +5,17 @@ const passport = require("passport");
 
 // Post Model
 const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
 
 // Validation
 const validatePostInput = require("../../validation/post");
 
-// @route   GET api/post/test
+// @route   GET api/posts/test
 // @desc    Test post route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Post work!" }));
 
-// @route   Post api/post
+// @route   Post api/posts
 // @desc    Create post
 // @access  Private
 router.post(
@@ -31,10 +32,85 @@ router.post(
     const newPost = new Post({
       text: req.body.text,
       name: req.body.name,
-      avatar: req.body.name,
+      avatar: req.body.avatar,
       user: req.user.id
     });
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   GET api/posts
+// @desc    GET post
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: "No post found" }));
+});
+
+// @route   GET api/posts/:id
+// @desc    GET post
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "Post not found with that id" })
+    );
+});
+
+// @route   DELETE api/posts/:id
+// @desc    DELETE post
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notautorized: "User not authorized" });
+          }
+
+          // DELETE
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
+
+// @route   POST api/posts/like/:id
+// @desc    POST like
+// @access  Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.like.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: "User already liked this post" });
+          }
+
+          // Add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
   }
 );
 
