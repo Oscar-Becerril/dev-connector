7c7507406c3ebd2226f5f4864fb0601daec5a837
oscar-becerril <oskbrrlz@gmail.com>
2019-05-01 10:00:29 -0500

    almost finished

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,11 +1,17 @@
 // TEST
 export const TEST_DISPATCH = "TEST_DISPATCH";
 
+////////////////////// ERROR //////////////////////
+
 // GET ERROR
 export const GET_ERRORS = "GET_ERRORS";
 
+///////////////////// USER //////////////////////////
+
 // SET current user
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+
+/////////////////// PROFILE /////////////////////////
 
 // GET PROFILE
 export const GET_PROFILE = "GET_PROFILE";
@@ -21,3 +27,20 @@ export const CLEAR_CURRENT_PROFILE = "CLEAR_CURRENT_PROFILE";
 
 // GET PROFILES
 export const GET_PROFILES = "GET_PROFILES";
+
+//////////////////// POST /////////////////////////
+
+// POST LOADING
+export const POST_LOADING = "POST_LOADING";
+
+// GET POSTS
+export const GET_POSTS = "GET_POSTS";
+
+// GET POST
+export const GET_POST = "GET_POST";
+
+// ADD POST
+export const ADD_POST = "ADD_POST";
+
+// DELETE POST
+export const DELETE_POST = "DELETE_POST";
